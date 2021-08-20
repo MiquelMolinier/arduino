2b7786eb71a88046973165845f2084d845484fe8
MiquelMolinier <miquelmolinier@gmail.com>
2021-08-19 22:20:54 -0500

    control PID position

--- pruebas/dcmotor/dcmotor.ino
+++ pruebas/dcmotor/dcmotor.ino
@@ -1,13 +1,16 @@
+#include <util/atomic.h>
+
 #define ENCA 2
 #define ENCB 3
 #define IDX 4
 #define IN1 13
 #define IN2 12
 #define ENA 5
-int pos = 0;
+
+volatile int posi = 0;
 long prevT = 0;
-float eprev = 0;
-float eintegral = 0;
+float eprev = 0.0;
+float eintegral = 0.0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -17,27 +20,50 @@ void setup() {
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   attachInterrupt(0, readEncoder, RISING);
-  
+  Serial.println("target pos targetprev");
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  setMotor(1, 127, ENA, IN1, IN2);
-  delay(200);
-  Serial.println(pos);
-  setMotor(-1, 127, ENA, IN1, IN2);
-  delay(200);
-  Serial.println(pos);
-  setMotor(0, 127, ENA, IN1, IN2);
-  delay(200);
-  Serial.println(pos);
+  // constantes PID
+  int target = analogRead(0);
+  float kp = 1.25;
+  float kd = 0.125;
+  float ki = 0.0;
+  long currT = micros();
+  float deltaT = ((float)(currT-prevT))/1.0e6;
+  prevT = currT;
+  int pos = 0; 
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
+  }
+  int e = pos - target;
+  float dedt = (e-eprev)/deltaT;
+  eintegral = eintegral + e*deltaT;
+  float u = kp*e +kd*dedt + ki*eintegral;
+  float pwm = fabs(u);
+  if(pwm >255){
+    pwm = 255;
+  }
+  int dir = 1;
+  if(u<0){
+    dir = -1;
+  }
+  setMotor(dir,pwm, ENA, IN1, IN2);
+  eprev = e;
+  Serial.print(1200);
+  Serial.print(" ");
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
 }
 void readEncoder(){
   int b = digitalRead(ENCB);
   if (b>0){
-    pos--; // CW rotate one way
+    posi--; // CW
   }
   else{
-    pos++; // CCW rotate other way
+    posi++; // CCW
   }
 }
 void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
